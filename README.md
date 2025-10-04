1e0c8f5ccc4429b5098f0a453a25e8e23ebbe264
Alice <ajiang7585@gmail.com>
2025-10-04 19:23:17 +0000

    put in a database to store quotes

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ For Tech-nol-hack 2025 at Carleton
 ## Ideas
 - a website
 - social media platform 
+- database to hold some info?
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const app = express();
+const db = new sqlite3.Database("quotes.db");
+
+app.use(express.json());
+
+//Create table
+db.run("CREATE TABLE IF NOT EXISTS quotes (id INTEGER PRIMARY KEY, text TEXT)");
+
+//Add a quote
+app.post("/quotes", (req, res) => {
+    db.run("INSERT INTO quotes (text) VALUES (?)", [req.body.text], function(err) {
+        if (err) return res.status(500).send(err.message);
+        res.json({id: this.lastID});
+    });
+});
+
+//Get all quotes
+app.get("/quotes", (req, res) => {
+    db.all("SELECT * FROM quotes", [], (err, rows) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(rows);
+    });
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
